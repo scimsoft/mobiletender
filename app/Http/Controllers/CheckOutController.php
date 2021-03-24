08c554f3e096ec6b1e8d96528083c862c3966479
Gerrit <gerrit.sevilla@gmail.com>
2021-03-24 19:23:39 +0100

    - add online payment for copas scenario

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -18,96 +18,61 @@ class CheckOutController extends Controller
     use SharedTicketTrait;
     use PrinterTrait;
 
-    public function checkout(){
+    public function checkout()
+    {
         $sharedTicketID = Session::get('ticketID');
-        $totalBasketPrice= $this->getSumTicketLines($sharedTicketID);
+        $totalBasketPrice = $this->getSumTicketLines($sharedTicketID);
         $newLinesPrice = $this->getSumNewTicketLines($sharedTicketID);
-        $tablenames= DB::select('select id,name from places order by id');
-       return view('order.checkout',compact(['totalBasketPrice','newLinesPrice','tablenames']));
+        $tablenames = DB::select('select id,name from places order by id');
+        return view('order.checkout', compact(['totalBasketPrice', 'newLinesPrice', 'tablenames']));
     }
 
-    public function confirmForTable($table_number){
-       $ticketID = Session::get('ticketID');
-       $this->moveTable($ticketID,$table_number);
-       Session::put('tableNumber',$table_number);
-       Session::put('ticketID',$table_number);
-       return $this->printOrder($table_number);
+    public function confirmForTable($table_number)
+    {
+        $ticketID = Session::get('ticketID');
+        $this->moveTable($ticketID, $table_number);
+        Session::put('tableNumber', $table_number);
+        Session::put('ticketID', $table_number);
+        return $this->printOrder($table_number);
     }
-    public function printOrderEfectivo($ticketID){
-        $this->footer='PAGAR en Efectivo';
-        $this->printFastOrder($ticketID);
-        return redirect()->route('order');
-    }
-         public function printOrderTarjeta($ticketID){
-             $this->footer='PAGAR con Tarjeta';
-             $this->printFastOrder($ticketID);
-             return redirect()->route('order');
-         }
-    public function printOrderOnline($ticketID){
-        $this->footer='PAGADO online';
-        $this->printFastOrder($ticketID);
-        return redirect()->route('order');
-    }
-
-         public function printFastOrder($ticketID){
-             $ticket= $this->getTicket($ticketID);
-             $header="Mesa: ".$ticketID;
-             try{
-                 $this->printBill($header,$this->getUnprintedTicetLines($ticket));
-                 Session::flash('status','Su pedido se esta preparando');
-             }catch (\Exception $e){
-                 Session::flash('error','No se ha podido imprimir el ticket. Por favor avisa a nuestro personal.');
-                 Log::error("Error Printing printerbridge error msg:" .$e);
-             }
-
-             $this->updateOpenTable($this->createEmptyTicket(),Session::get('ticketID'));
-
-         }
 
     public function printOrder($ticketID)
     {
-        $ticket= $this->getTicket($ticketID);
-        $header="Mesa: ".$ticketID;
-        try{
-            $this->printTicket($header,$this->getUnprintedTicetLines($ticket));
-            $this->setUnprintedTicketLinesAsPrinted($ticket,$ticketID);
-            if(config('customoptions.clean_table_after_order')){
-                $this->updateOpenTable($this->createEmptyTicket(),Session::get('ticketID'));
+        $ticket = $this->getTicket($ticketID);
+        $header = "Mesa: " . $ticketID;
+        try {
+            $this->printTicket($header, $this->getUnprintedTicetLines($ticket));
+            $this->setUnprintedTicketLinesAsPrinted($ticket, $ticketID);
+            if (config('customoptions.clean_table_after_order')) {
+                $this->updateOpenTable($this->createEmptyTicket(), Session::get('ticketID'));
             }
-        }catch (\Exception $e){
-            Session::flash('error','No se ha podido imprimir el ticket. Por favor avisa a nuestro personal.');
-            Log::error("Error Printing printerbridge error msg:" .$e);
+        } catch (\Exception $e) {
+            Session::flash('error', 'No se ha podido imprimir el ticket. Por favor avisa a nuestro personal.');
+            Log::error("Error Printing printerbridge error msg:" . $e);
         }
         Log::debug('return from printOrder');
         return redirect()->route('basket');
     }
 
-    public function setPickUpId(){
-        $pickup_ID=  DB::table('pickup_number')->max('id');
-        $pickup_ID = $pickup_ID +1;
-        DB::statement("UPDATE pickup_number set id = ".$pickup_ID.";");
-        $this->moveTable(Session::get('ticketID'),"Pick UP:". $pickup_ID);
+    private function getUnprintedTicetLines(SharedTicket $ticket)
+    {
+        $lines_to_print = null;
+        foreach ($ticket->m_aLines as $ticket_line) {
+            // Log::debug('line updated:'.$ticket_line->attributes->updated);
+            if ($ticket_line->attributes->updated == 1) {
+                $lines_to_print[] = $ticket_line;
 
-        Session::put('tableNumber',$pickup_ID);
-        Session::put('ticketID',$pickup_ID);
-        return redirect()->route('pay');
+            }
+        }
+
+        return $lines_to_print;
     }
-
-    public function pay(){
-        $sharedTicketID = Session::get('ticketID');
-        //dd($sharedTicketID);
-        $totalBasketPrice= $this->getSumTicketLines($sharedTicketID);
-        $newLinesPrice = $this->getSumNewTicketLines($sharedTicketID);
-        $tablenames= DB::select('select id,name from places order by id');
-        return view('order.pay',compact(['totalBasketPrice','newLinesPrice','tablenames']));
-    }
-
 
     /**
      * @param $ticket_lines
      * @return array|null
      */
-    private function setUnprintedTicketLinesAsPrinted(SharedTicket $ticket,$ticketID)
+    private function setUnprintedTicketLinesAsPrinted(SharedTicket $ticket, $ticketID)
     {
         $lines_to_print = null;
         foreach ($ticket->m_aLines as $ticket_line) {
@@ -116,21 +81,68 @@ class CheckOutController extends Controller
                 $ticket_line->setPrinted();
             }
         }
-        $this->updateOpenTable($ticket,$ticketID);
+        $this->updateOpenTable($ticket, $ticketID);
         return $lines_to_print;
     }
 
-    private function getUnprintedTicetLines(SharedTicket $ticket){
-        $lines_to_print = null;
-        foreach ($ticket->m_aLines as $ticket_line) {
-           // Log::debug('line updated:'.$ticket_line->attributes->updated);
-            if ($ticket_line->attributes->updated == 1) {
-                $lines_to_print[] = $ticket_line;
+    public function printOrderEfectivo($ticketID)
+    {
+        $this->footer = 'PAGAR en Efectivo';
+        $this->printFastOrder($ticketID);
+        return redirect()->route('order');
+    }
 
-            }
+    public function printFastOrder($ticketID)
+    {
+        $ticket = $this->getTicket($ticketID);
+        $header = "Mesa: " . $ticketID;
+        try {
+            $this->printBill($header, $this->getUnprintedTicetLines($ticket));
+            Session::flash('status', 'Su pedido se esta preparando');
+        } catch (\Exception $e) {
+            Session::flash('error', 'No se ha podido imprimir el ticket. Por favor avisa a nuestro personal.');
+            Log::error("Error Printing printerbridge error msg:" . $e);
         }
 
-        return $lines_to_print;
+        $this->updateOpenTable($this->createEmptyTicket(), Session::get('ticketID'));
+
+    }
+
+    public function printOrderTarjeta($ticketID)
+    {
+        $this->footer = 'PAGAR con Tarjeta';
+        $this->printFastOrder($ticketID);
+        return redirect()->route('order');
+    }
+
+    public function printOrderOnline($ticketID)
+    {
+        $this->footer = 'PAGADO online';
+        $this->printFastOrder($ticketID);
+        return redirect()->route('order');
+    }
+
+    public function setPickUpId()
+    {
+        $pickup_ID = DB::table('pickup_number')->max('id');
+        $pickup_ID = $pickup_ID + 1;
+        DB::statement("UPDATE pickup_number set id = " . $pickup_ID . ";");
+        $this->moveTable(Session::get('ticketID'), $pickup_ID);
+
+        Session::put('tableNumber', $pickup_ID);
+        Session::put('ticketID', $pickup_ID);
+        Session::put('isPickup', true);
+        return redirect()->route('pay');
+    }
+
+    public function pay()
+    {
+        $sharedTicketID = Session::get('ticketID');
+        //dd($sharedTicketID);
+        $totalBasketPrice = $this->getSumTicketLines($sharedTicketID);
+        $newLinesPrice = $this->getSumNewTicketLines($sharedTicketID);
+        $tablenames = DB::select('select id,name from places order by id');
+        return view('order.pay', compact(['totalBasketPrice', 'newLinesPrice', 'tablenames']));
     }
 
 
