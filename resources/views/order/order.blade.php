542bcd8939f4a82660d320db751005444955c293
Gerrit <gerrit.sevilla@gmail.com>
2021-05-03 14:21:46 +0200

    modal products add-on scrollable

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -119,8 +119,8 @@
 
 
     <div class="modal fade" id="selectAddOnModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-         aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
+         aria-hidden="true" >
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLongTitle">Con</h5>
