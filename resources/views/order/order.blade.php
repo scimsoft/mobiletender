0f52126714c0d99d9a91891e60f24e4472d7cc8f
Gerrit <gerrit.sevilla@gmail.com>
2021-05-06 20:23:14 +0200

    select table page table in red if table is occupied

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -120,7 +120,7 @@
 
     <div class="modal fade" id="selectAddOnModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
          aria-hidden="true" >
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document" style="-webkit-overflow-scrolling: touch;">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLongTitle">Con</h5>
