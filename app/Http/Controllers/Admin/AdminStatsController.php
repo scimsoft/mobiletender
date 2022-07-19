662dfecce6e27088c2d28ca8d22409c5c3784ac3
Gerrit Schimmelpenninck <gerrit@apicenter.io>
2022-07-19 22:05:38 +0200

    mejoras en final blade

--- app/Http/Controllers/Admin/AdminStatsController.php
+++ app/Http/Controllers/Admin/AdminStatsController.php
@@ -10,26 +10,38 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Traits\UnicentaPayedTrait;
 use function compact;
 use Illuminate\Support\Facades\DB;
 
 class AdminStatsController extends Controller
 {
+    use UnicentaPayedTrait;
 
     public function index()
     {
-        $ventaLinesHoy = DB::select('SELECT payments.PAYMENT, SUM(ticketlines.UNITS*ticketlines.PRICE*1.1) AS TOTAL
+        $ventaLinesHoy = DB::select("SELECT payments.PAYMENT, SUM(ticketlines.UNITS*ticketlines.PRICE*1.1) AS TOTAL
 FROM tickets, ticketlines, receipts, closedcash,payments
 WHERE ticketlines.TICKET = receipts.ID
 AND closedcash.MONEY = receipts.MONEY
 AND ticketlines.TICKET = tickets.Id
 and tickets.Id = payments.RECEIPT
-AND date(receipts.DATENEW) 
->= (CURDATE())
-GROUP BY payments.PAYMENT');
+AND date(receipts.DATENEW)>= (CURDATE())
+AND time(receipts.DATENEW) >= '10:00'
+GROUP BY payments.PAYMENT");
 
+        $ventaLinesHoyNight = DB::select("SELECT payments.PAYMENT, SUM(ticketlines.UNITS*ticketlines.PRICE*1.1) AS TOTAL
+FROM tickets, ticketlines, receipts, closedcash,payments
+WHERE ticketlines.TICKET = receipts.ID
+AND closedcash.MONEY = receipts.MONEY
+AND ticketlines.TICKET = tickets.Id
+and tickets.Id = payments.RECEIPT
+AND date(receipts.DATENEW)>= (CURDATE())
+AND time(receipts.DATENEW) <= '10:00'
+GROUP BY payments.PAYMENT");
+        $cajaActual = $this->getClosedCash();
 
-        $categoriesHoy=DB::select('SELECT  categories.name as NAME, COUNT(categories.name) AS CHECKS, SUM(ticketlines.UNITS*ticketlines.PRICE*1.1) AS TOTAL
+        $categoriesHoy = DB::select('SELECT  categories.name as NAME, COUNT(categories.name) AS CHECKS, SUM(ticketlines.UNITS*ticketlines.PRICE*1.1) AS TOTAL
 FROM tickets, ticketlines, receipts,
  closedcash,products,categories
 WHERE ticketlines.TICKET = receipts.ID
@@ -39,6 +51,7 @@ AND ticketlines.PRODUCT = products.ID
 AND products.CATEGORY = categories.ID
 AND date(receipts.DATENEW) >= (curdate())
 GROUP BY NAME');
+
         $ventaPorDias = DB::select('SELECT date(receipts.Datenew) as daynumber, COUNT(DISTINCT(receipts.ID)) AS CHECKS, SUM(ticketlines.UNITS*ticketlines.PRiCE*1.1) AS TOTAL
  FROM tickets, ticketlines, receipts, closedcash WHERE ticketlines.TICKET = receipts.ID
 AND closedcash.MONEY = receipts.MONEY
@@ -47,14 +60,20 @@ AND receipts.DATENEW >= CURDATE()-13
 GROUP BY daynumber');
 
 
-        $total= 0;
+        $totalDay = 0;
         foreach ($ventaLinesHoy as $ventaLineHoy) {
-            $total += $ventaLineHoy->TOTAL;
+            $totalDay += $ventaLineHoy->TOTAL;
+
+
+        }
+        $totalNight = 0;
+        foreach ($ventaLinesHoyNight as $ventaLineaHoyNight) {
+            $totalNight += $ventaLineaHoyNight->TOTAL;
 
 
         }
 
-        return view('admin.stats.index',compact(['ventaLinesHoy','categoriesHoy','ventaPorDias','total']));
+        return view('admin.stats.index', compact(['ventaLinesHoy', 'ventaLinesHoyNight', 'cajaActual','categoriesHoy', 'ventaPorDias', 'totalDay','totalNight']));
     }
 
 
--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -19,20 +19,36 @@
 
                         <div>&nbsp;</div>
                         <table class="table table-sm">
-                            <tr><td colspan="2" class="text-center"><h4>Venta de Hoy</h4></td></tr>
+
+                            <tr class="bg-light"><td colspan="2" class="text-center"><h4>Caja Actual</h4></td></tr>
+
+                            @foreach($cajaActual as $cajaActualLine)
+                                <tr><td>{{$cajaActualLine->payment}}</td><td>@money($cajaActualLine->total)</td></tr>
+                            @endforeach
+                            <tr><td colspan="2" class="">&nbsp;</td></tr>
+
+                            <tr class="bg-light"><td colspan="2" class="text-center"><h4>Venta de Hoy por el dia</h4></td></tr>
 
                             @foreach($ventaLinesHoy as $ventaLine)
                                 <tr><td>{{$ventaLine->PAYMENT}}</td><td>@money($ventaLine->TOTAL)</td></tr>
                             @endforeach
-                                <tr><td><b>TOTAL</b></td><td><b>@money($total)</b></td></tr>
+                                <tr><td><b>TOTAL</b></td><td><b>@money($totalDay)</b></td></tr>
                             <tr><td colspan="2" class="">&nbsp;</td></tr>
-                            <tr><td colspan="2" class="text-center"><h4>Venta por Categoria</h4></td></tr>
+                            <tr class="bg-light"><td colspan="2" class="text-center"><h4>Venta de Hoy por la noche</h4></td></tr>
+
+                            @foreach($ventaLinesHoyNight as $ventaLine)
+                                <tr><td>{{$ventaLine->PAYMENT}}</td><td>@money($ventaLine->TOTAL)</td></tr>
+                            @endforeach
+                            <tr><td><b>TOTAL</b></td><td><b>@money($totalNight)</b></td></tr>
+                            <tr><td colspan="2" class="">&nbsp;</td></tr>
+
+                            <tr class="bg-light"><td colspan="2" class="text-center"><h4>Venta por Categoria</h4></td></tr>
                             @foreach($categoriesHoy as $categorie)
                                 <tr><td>{{$categorie->NAME}}</td><td>@money($categorie->TOTAL)</td></tr>
 
                                 @endforeach
                             <tr><td colspan="2" class="">&nbsp;</td></tr>
-                            <tr><td colspan="2" class="text-center"><h4>Venta por dia</h4></td></tr>
+                            <tr class="bg-light"> <td colspan="2" class="text-center"><h4>Venta por dia</h4></td></tr>
                             @foreach($ventaPorDias as $ventaPorDia)
                                 <tr><td>{{Carbon\Carbon::parse($ventaPorDia->daynumber)->format('l')}}&nbsp;- &nbsp;{{$ventaPorDia->daynumber}}</td><td>@money($ventaPorDia->TOTAL)</td></tr>
 
